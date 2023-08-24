2aaa15dbe57e946f117cee694dce069ef77365e8
Sakaguchi <19sakaguchi@gmail.com>
2023-08-24 10:30:24 +0900

    mongoDBの導入

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config/dev')
+const model = require('./model/image')
+
+mongoose.connect(config.DB_URI)
 
 const app = express()
+
+app.get('/import', function(req, res) {
+    res.json({'success': true})
+})
 
 app.listen('3001', function() {
     console.log('I am running!')
---
+++ backend/model/image.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const ObjectId = Schema.ObjectId
+
+const imageSchema = mongoose.Schema({
+    id  : ObjectId,
+    name : { type : String, required : true }, 
+    state : String
+})
+
+module.exports = mongoose.model('Image', imageSchema)
