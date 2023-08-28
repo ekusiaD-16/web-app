aaa0c9dd28e699fec4c22420f67ebe639a57b944
Sakaguchi <19sakaguchi@gmail.com>
2023-08-28 10:49:01 +0900

    コメントアウト

--- frontend/src/app/service/http-client.service.ts
+++ frontend/src/app/service/http-client.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 
-import { Image } from '../Image';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -11,23 +9,7 @@ export class HttpClientService {
 
   host : string = 'http://localhost:4200/app'
 
-  // images : Image[] = []
-  // image : Image = new Image('_id','name','path','state','_v')
-
   constructor(private httpClient : HttpClient) { }
-
-  // getImages(callback : any) {
-  //   return this.httpClient.get(this.host + '/api/v1/images')
-  //   .subscribe(
-  //     (res) => {
-  //       const response : any = res
-  //       callback(response)
-  //     },
-  //     (error) => {
-  //       console.log(error)
-  //     }
-  //   )
-  // }
 
   getImages() : Observable<any> {
     return this.httpClient.get(this.host + '/api/v1/images')
