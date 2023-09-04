6905ff8986a3d4eaadcbc80fbd0027460d4afd52
Sakaguchi <19sakaguchi@gmail.com>
2023-09-04 15:14:06 +0900

    backend resize,rotate API

--- backend/routes/editor.js
+++ backend/routes/editor.js
@@ -116,6 +116,128 @@ router.post('/toGray', function(req, res) {
     }}
 )
 
+router.post('/resize', function(req, res) {
+    if(!req) {
+        const editError = new Error.EditError('request is null')
+        res.status(500).send({ name : editError.name , message : editError.message })
+    }
+    else {
+        //DBから該当Imageを取得
+        const imageId = req.body.imageId
+        const resizeRate = req.body.resizeRate
+        Image.findById(imageId).then(
+            (foundImage) => {
+                if(!foundImage) {
+                    throw new Error.ImageNotFoundError('Image not found target id:'+imageId)
+                }
+                else {
+                    // toGray処理を実行
+                    const url = 'http://127.0.0.1:5000/process/resize'
+                    const data = JSON.stringify( { 'image' : foundImage , 'params' : { 'resizeRate':resizeRate } })
+                    fetch(url, {
+                        method:'POST',
+                        body:data,
+                        headers:{"Content-Type": "application/json"}
+                    }
+                    ).then(
+                        (response) => {
+                            console.log('recieved response',response)
+                            res.json({'result':true})
+                        },
+                        (err) => {
+                            console.error(err)
+                            const editError = new Error.EditError('request is null')
+                            res.status(500).send({ name : editError.name , message : editError.message })
+                        }
+                    )
+                }
+            },
+            (err) => {
+                throw new Error.ImageNotFoundError('Image not found target id:'+imageId, err)
+            },
+        )
+        .catch( (err) => {
+            console.error(err)
+            if(err instanceof Error.ImageNotFoundError) {
+                res.status(404).send({ name : err.name , message : err.message })
+            }
+            if(err instanceof Error.IllegalImageError) {
+                res.status(500).send({ name : err.name , message : err.message })
+            }
+            if(err instanceof Error.RegisterError) {
+                res.status(500).send({ name : err.name , message : err.message })
+            }
+            if(err instanceof Error.EditError) {
+                res.status(500).send({ name : err.name , message : err.message })
+            }
+            else {
+                res.status(500).send({ name : 'UnknownError' , message : 'samething happen' })
+            }
+        } )
+    }}
+)
+
+router.post('/rotate', function(req, res) {
+    if(!req) {
+        const editError = new Error.EditError('request is null')
+        res.status(500).send({ name : editError.name , message : editError.message })
+    }
+    else {
+        //DBから該当Imageを取得
+        const imageId = req.body.imageId
+        const angle = req.body.angle
+        Image.findById(imageId).then(
+            (foundImage) => {
+                if(!foundImage) {
+                    throw new Error.ImageNotFoundError('Image not found target id:'+imageId)
+                }
+                else {
+                    // toGray処理を実行
+                    const url = 'http://127.0.0.1:5000/process/rotate'
+                    const data = JSON.stringify( { 'image' : foundImage , 'params' : { 'angle':angle } })
+                    fetch(url, {
+                        method:'POST',
+                        body:data,
+                        headers:{"Content-Type": "application/json"}
+                    }
+                    ).then(
+                        (response) => {
+                            console.log('recieved response',response)
+                            res.json({'result':true})
+                        },
+                        (err) => {
+                            console.error(err)
+                            const editError = new Error.EditError('request is null')
+                            res.status(500).send({ name : editError.name , message : editError.message })
+                        }
+                    )
+                }
+            },
+            (err) => {
+                throw new Error.ImageNotFoundError('Image not found target id:'+imageId, err)
+            },
+        )
+        .catch( (err) => {
+            console.error(err)
+            if(err instanceof Error.ImageNotFoundError) {
+                res.status(404).send({ name : err.name , message : err.message })
+            }
+            if(err instanceof Error.IllegalImageError) {
+                res.status(500).send({ name : err.name , message : err.message })
+            }
+            if(err instanceof Error.RegisterError) {
+                res.status(500).send({ name : err.name , message : err.message })
+            }
+            if(err instanceof Error.EditError) {
+                res.status(500).send({ name : err.name , message : err.message })
+            }
+            else {
+                res.status(500).send({ name : 'UnknownError' , message : 'samething happen' })
+            }
+        } )
+    }}
+)
+
 function zoom(image, zoomRate, newName) {
     // TODO OpenCV.js で処理
     const base = newName || image.name + '_' + 'zoom' + '_' + zoomRate
