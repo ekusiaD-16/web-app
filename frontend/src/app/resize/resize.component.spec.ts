1d051127899de08a0449063c27d95383f7e32b43
Sakaguchi <19sakaguchi@gmail.com>
2023-10-18 17:05:39 +0900

    resizeComponent test

--- frontend/src/app/resize/resize.component.spec.ts
+++ frontend/src/app/resize/resize.component.spec.ts
@@ -1,29 +1,111 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { ResizeComponent } from './resize.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
+import { HttpClientService } from '../service/http-client.service';
+import { Observable } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 describe('ResizeComponent', () => {
   let component: ResizeComponent;
   let fixture: ComponentFixture<ResizeComponent>;
+  let location: Location;
+
+  @Component({
+    template:`stub`
+  })
+  class StubListComponent {}
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
+        RouterTestingModule.withRoutes(
+          [ { path: 'list', component: StubListComponent } ]
+        ),
         FormsModule,
       ],
-      declarations: [ ResizeComponent ]
+      declarations: [ ResizeComponent ],
+      providers: [
+        HttpClientService,
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ResizeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    location = TestBed.get(Location);
+    });
 
-  it('should create', () => {
+  test('constructor', () => {
     expect(component).toBeTruthy();
   });
+
+  test('should have input', () => {
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input).toBeTruthy();
+  });
+
+  test('should have excute button', () => {
+    const elements = fixture.nativeElement.querySelector('button');
+    expect(elements.textContent).toContain('excute')
+  });
+
+  test('should not have error message', () => {
+    // 初期状態 error messageなし
+    const normal_message = fixture.nativeElement.querySelector('p');
+    expect(normal_message).toBeFalsy();
+  });
+
+  test('#正常系 messageが出ず、/listへredirectする', fakeAsync( () => {
+    // httpClientService.sendResize()をmock
+    const httpClientService = TestBed.inject(HttpClientService);
+    jest.spyOn(httpClientService,'sendResize').mockImplementation( (editorJson:object) => {
+      return new Observable(observer => { observer.next({'result':true}) })
+    })
+    // 正常時 入力
+    component.imageId = '652f47086056dec9bd72b4d5';
+    component.resizeRate = 0.5;
+    // excute button click
+    let button = fixture.nativeElement.querySelector('button');
+    button.click();
+    tick();
+    // message=='' path=='/list'
+    expect(component.message).toBe('')
+    expect(location.path()).toBe('/list')
+  }));
+
+  test('#異常系(resizeRate=0) 入力値が事前チェックに落ちる', fakeAsync( () => {
+    // 異常時 入力
+    component.imageId = '652f47086056dec9bd72b4d5';
+    component.resizeRate = 0;
+    // excute button click
+    let button = fixture.nativeElement.querySelector('button');
+    button.click();
+    tick();
+    // message=='Error: invalid number'
+    expect(component.message).toBe('Error: invalid number');
+  }));
+
+  test('#異常系 httpClientServiceからerrorが返ってくる', fakeAsync( () => {
+    // httpClientService.sendResize()をmock errorを返す
+    const httpClientService = TestBed.inject(HttpClientService);
+    jest.spyOn(httpClientService,'sendResize').mockImplementation( (editorJson:object) => {
+      return new Observable(observer => { observer.error(new Error('httpClientServiceError').message) })
+    })
+    // 正常時 入力
+    component.imageId = '652f47086056dec9bd72b4d5';
+    component.resizeRate = 0.5;
+    // excute button click
+    let button = fixture.nativeElement.querySelector('button');
+    button.click();
+    tick();
+    // message=='httpClientServiceError'
+    expect(component.message).toBe('httpClientServiceError');
+  }));
+
 });
