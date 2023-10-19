3e2b446e6b9965be573584de3dce2682106de857
Sakaguchi <19sakaguchi@gmail.com>
2023-10-19 11:59:11 +0900

    headerComponent test

--- frontend/src/app/header/header.component.spec.ts
+++ frontend/src/app/header/header.component.spec.ts
@@ -1,29 +1,79 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 import { HeaderComponent } from './header.component';
 import { ImgUploadComponent } from '../img-upload/img-upload.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let location: Location;
+
+  @Component({
+    selector: 'app-img-upload',
+    template: 'stub'
+  })
+  class StubImgUploadComponent {}
+
+  @Component({
+    template:`stub`
+  })
+  class StubListComponent {}
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [ { path: 'list', component: StubListComponent } ]
+        ),
+      ],
       declarations: [
         HeaderComponent,
-        ImgUploadComponent,
+        StubImgUploadComponent,
+       ],
+       providers: [
+        { provide: ImgUploadComponent, useValue: StubImgUploadComponent }
        ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    location = TestBed.get(Location);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('constructor', () => {
     expect(component).toBeTruthy();
   });
+
+  test('shoud have elements', () => {
+    const elements = fixture.nativeElement;
+    expect(elements.textContent).toContain('File');
+    expect(elements.textContent).toContain('Upload');
+    expect(elements.textContent).toContain('List');
+  });
+
+  test('should have link to list', () => {
+    const listLink = fixture.nativeElement.querySelector('.list');
+    expect(listLink.textContent).toContain('List');
+
+    expect(location.path()).toBe('');
+    listLink.click();
+    expect(location.path()).toBe('/list');
+  });
+
+  test('should have link to upload', () => {
+    const uploadLink = fixture.nativeElement.querySelector('.upload');
+    expect(uploadLink.textContent).toContain('Upload');
+
+    uploadLink.click();
+    const modal = fixture.nativeElement.querySelector('#uploadModal');
+    expect(modal).toBeTruthy();
+    const imgUploadComponent = TestBed.get(ImgUploadComponent);
+    expect(imgUploadComponent).toBeTruthy();
+  });
+
 });
